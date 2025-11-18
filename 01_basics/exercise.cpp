1fc7bc15bbbc7bf8a8d6a454118719e05f1df7eb
MaSeungHyun <seunghyun.ma@grapicar.com>
2025-11-18 10:52:33 +0900

    solved: 변수, 입출력, 연산 문제 풀이

--- 01_basics/exercise.cpp
+++ 01_basics/exercise.cpp
@@ -13,9 +13,36 @@
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성하세요
+    // 문제 1
+    cout << "두 수를 입력하세요: ";
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    cout << "+ : " << num1 + num2 << endl;
+    cout << "- : " << num1 - num2 << endl;
+    cout << "* : " << num1 * num2 << endl;
+    if(num2 != 0) {
+        cout << "/ : " << num1 / num2 << endl;
+    } else {
+        cout << "/ : 0으로 나눌 수 없습니다." << endl;
+    }
     
+    // 문제 2
+    cin.ignore();
+    cout << "이름을 입력하세요: ";
+    string name;
+    getline(cin, name);
+    cout << "나이를 입력하세요: ";
+    int age;
+    cin >> age;
+    cout << "안녕하세요, " << name << "님! " << age << "살이시군요!" << endl;
     
+    // 문제 3
+    cout << "섭씨 온도를 입력하세요: ";
+    double celsius;
+    cin >> celsius;
+    cout << "섭씨 " << celsius << "도는 화씨 " << celsius * 9/5 + 32 << "도 입니다." << endl;
+
     return 0;
 }
 
